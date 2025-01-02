e626155b93a40fc5915646eeb5348dc73e5479d2
Jaros-777 <filip.jarocki@wp.pl>
2025-01-02 19:21:21 +0100

    End of free plan on SupaBase

--- shop/src/FilterProductsPage.jsx
+++ shop/src/FilterProductsPage.jsx
@@ -34,6 +34,12 @@ function FilterProductPage() {
       </div>
       <div id="home-page-container">
         <div id="products-container">
+        <div style={{gridColumn:2, width: "100%", display:"flex",flexDirection:"column", justifyContent:"center", alignItems:"center", border:"2px solid black",borderRadius:"20px", padding: "2vh"}}>
+        <p>
+        The website is under reconstruction due to the lack of costs for the database. To view the website and learn about its functionality, please visit GitHub</p>
+        <p>Sorry for the inconvenience</p>
+        <button style={{marginTop:"2vh", border:"2px solid black",borderRadius:"20px", padding:"2vh"}}> <a href="https://github.com/Jaros-777/Valufi">Go to GitHub</a></button>
+      </div>
           {viewProductsList.map((e)=>(
             <ProductCard key={e.id} name={e.name} price={e.price} img={e.imgURL} id={e.id}></ProductCard>
           ))}
